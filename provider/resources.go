40681958eab772b33ffe56bbc5849101456a21a6
huaxk <huaxk@163.com>
2021-06-13 21:21:45 +0800

    First commit

--- provider/resources.go
+++ provider/resources.go
@@ -12,27 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package xyz
+package proxmox
 
 import (
 	"fmt"
 	"path/filepath"
 	"unicode"
 
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/Telmate/terraform-provider-proxmox/proxmox"
+	"github.com/huaxk/pulumi-proxmox/provider/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
-	shimv1 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v1"
+	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
-	"github.com/terraform-providers/terraform-provider-xyz/xyz"
 )
 
 // all of the token components used below.
 const (
 	// packages:
-	mainPkg = "xyz"
+	mainPkg = "proxmox"
 	// modules:
 	mainMod = "index" // the y module
 )
@@ -91,17 +90,17 @@ var managedByPulumi = &tfbridge.DefaultInfo{Value: "Managed by Pulumi"}
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
-	p := shimv1.NewProvider(xyz.Provider().(*schema.Provider))
+	p := shimv2.NewProvider(proxmox.Provider())
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
 		P:           p,
-		Name:        "xyz",
-		Description: "A Pulumi package for creating and managing xyz cloud resources.",
-		Keywords:    []string{"pulumi", "xyz"},
+		Name:        "proxmox",
+		Description: "A Pulumi package for creating and managing proxmox cloud resources.",
+		Keywords:    []string{"pulumi", "proxmox"},
 		License:     "Apache-2.0",
 		Homepage:    "https://pulumi.io",
-		Repository:  "https://github.com/pulumi/pulumi-xyz",
+		Repository:  "https://github.com/pulumi/pulumi-proxmox",
 		Config:      map[string]*tfbridge.SchemaInfo{
 			// Add any required configuration here, or remove the example below if
 			// no additional points are required.
@@ -113,19 +112,18 @@ func Provider() tfbridge.ProviderInfo {
 			// },
 		},
 		PreConfigureCallback: preConfigureCallback,
-		Resources:            map[string]*tfbridge.ResourceInfo{
+		Resources: map[string]*tfbridge.ResourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi type. Two examples
 			// are below - the single line form is the common case. The multi-line form is
 			// needed only if you wish to override types or other default options.
 			//
-			// "aws_iam_role": {Tok: makeResource(mainMod, "IamRole")}
-			//
-			// "aws_acm_certificate": {
-			// 	Tok: makeResource(mainMod, "Certificate"),
-			// 	Fields: map[string]*tfbridge.SchemaInfo{
-			// 		"tags": {Type: makeType(mainPkg, "Tags")},
-			// 	},
-			// },
+			"proxmox_vm_qemu": {Tok: makeResource(mainMod, "QemuVM")},
+			// "proxmox_qemu_disk": {Tok: makeResource(mainMod, "QemuDisk")},
+			"proxmox_lxc": {Tok: makeResource(mainMod, "LXCContainer")},
+			"proxmox_lxc_disk": {
+				Tok:                 makeResource(mainMod, "LXCDisk"),
+				DeleteBeforeReplace: true,
+			},
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi function. An example
--- sdk/go.mod
+++ sdk/go.mod
@@ -1,5 +1,5 @@
-module github.com/pulumi/pulumi-xyz/sdk
+module github.com/pulumi/pulumi-proxmox/sdk
 
-go 1.14
+go 1.16
 
 require github.com/pulumi/pulumi/sdk/v3 v3.4.0
